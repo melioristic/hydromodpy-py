ca49038f9a05a3d5a9ac54ede5f9ebe4827c23e4
Mohit Anand <mohit@Mohits-MacBook-Pro.local>
2020-05-16 18:10:30 +0530

    partially funcitonal sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 #sys.path.insert(0, os.path.abspath('/Users/mohit/github/hydromod-py/'))
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 
