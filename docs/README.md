34778e249bab9fba190c6a849b8f2e3dd52a9442
Mohit Anand <mohit@Mohits-MacBook-Pro.local>
2020-05-20 12:08:17 +0530

    update docs

--- docs/README.md
+++
@@ -1,2 +0,0 @@
-# hydromodpy -py
-A python package for hydrological modelling with a goal to expedite science
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-
-sys.path.insert(0, os.path.abspath('../')) 
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -25,20 +24,13 @@ author = 'Mohit Anand'
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark',  # to use .md along with .rst
-              'sphinx.ext.autodoc',  # import doc from docstrings
-              'sphinx.ext.linkcode',  # linking the source code on github
-              'sphinx_rtd_theme']  # to support Google style docstrings for autodoc
-
-master_doc = 'index'
-source_suffix = ['.rst', '.md']
-
-html_theme = "sphinx_rtd_theme"
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -60,15 +52,3 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-def linkcode_resolve(domain, info):
-    """To provide github source link for the methods
-    https://www.sphinx-doc.org/en/master/usage/extensions/linkcode.html
-    """
-
-    if domain != 'py':
-        return None
-    if not info['module']:
-        return None
-    filename = info['module'].replace('.', '/')
-    return "https://github.com/melioristic/hydromodpy-py/tree/master/{}.py".format(filename)
